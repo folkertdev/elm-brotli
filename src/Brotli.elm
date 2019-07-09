9e73e8d5a0250bbb0789245b7c49a8b3d61876f7
Folkert <folkert@folkertdev.nl>
2019-07-09 23:08:35 +0200

    fix doc example

--- src/Brotli.elm
+++ src/Brotli.elm
@@ -5,27 +5,28 @@ module Brotli exposing (decode)
 Brotli is a compression algorithm much like the one zip archives use, but better: it is faster, more compact, and specifically geared towards web content and English text.
 This package decodes `Bytes` sequences that are compressed with the brotli algorithm:
 
-import Bytes exposing (Bytes)
-import Bytes.Encode as Encode
-import Bytes.Decode as Decode
-import Brotli
+    import Bytes exposing (Bytes)
+    import Bytes.Encode as Encode
+    import Bytes.Decode as Decode
+    import Brotli
 
-bytes : List Int
-bytes = 
+    bytes : List Int
+    bytes =
     [ 27, 14, 0, 248, 37, 20, 82, 144, 66, 20, 169, 91, 100, 234, 20, 193 ]
 
-buffer : Bytes
-buffer = 
-    bytes 
-        |> List.map Encode.unsignedInt8 
-        |> Encode.sequence
-        |> Encode.encode
+    buffer : Bytes
+    buffer =
+    bytes
+    |> List.map Encode.unsignedInt8
+    |> Encode.sequence
+    |> Encode.encode
 
-result = 
-    Brotli.decode buffer 
-        |> Result.toMaybe
-        |> Maybe.andThen (\v -> Decode.decode (Decode.string (Bytes.width v)) v)
-        --> Just "this is a test\n"            
+    result =
+    Brotli.decode buffer
+    |> Result.toMaybe
+    |> Maybe.andThen (\\v -> Decode.decode (Decode.string (Bytes.width v)) v)
+    --> Just "this is a test\\n"
+
 
 ## Decode
 
@@ -44,30 +45,64 @@ If you ever run into an error, that probably means you've hit a bug. If you can,
 -}
 decode : Bytes -> Result String Bytes
 decode buffer =
-    case Internal.decode buffer of 
-        Err e -> 
-            Err ( renderError e) 
-        Ok v -> 
+    case Internal.decode buffer of
+        Err e ->
+            Err (renderError e)
+
+        Ok v ->
             Ok v
 
-renderError : Internal.Error -> String
-renderError error = 
-    case  error of
-        Internal.MaxDistanceTooSmall { actual, minimal } -> "maxDistance is too small: it should be at least" ++ String.fromInt minimal ++ ", but it is only " ++ String.fromInt actual
-        Internal.StateNotInitialized-> "State not initialized"
-        Internal.ReadAfterEnd-> "Read after end"
-        Internal.UnusedByteAfterEnd-> "Unused byte after end"
-        Internal.ExuberantNibble-> "Exuberant nibble"
-        Internal.ExuberantByte-> "Exuberant byte"
-        Internal.CorruptedReservedBit-> "Corrupted reserved bit"
-        Internal.CorruptedHuffmanHistogram-> "Corrupted Huffman histogram"
-        Internal.CustomError message -> message
-        Internal.UnusedSpace-> "Unused space"
-        Internal.NoHuffmanCode symbol -> "There is no huffman code for symbol " ++ String.fromInt symbol ++ ", it is outside of the alphabet!"
-        Internal.CorruptedContextMap-> "Corrupted ContextMap"
-        Internal.InvalidWindowBits-> "Invalid window bits"
-        Internal.InvalidMetablockLength-> "invalid MetaBlock length"
-        Internal.InvalidBackwardReference message -> message
-        Internal.UnalignedCopyBytes-> "Unaligned copy bytes"
-        Internal.CorruptedPaddingBits-> "Corrupted padding bits"
 
+renderError : Internal.Error -> String
+renderError error =
+    case error of
+        Internal.MaxDistanceTooSmall { actual, minimal } ->
+            "maxDistance is too small: it should be at least" ++ String.fromInt minimal ++ ", but it is only " ++ String.fromInt actual
+
+        Internal.StateNotInitialized ->
+            "State not initialized"
+
+        Internal.ReadAfterEnd ->
+            "Read after end"
+
+        Internal.UnusedByteAfterEnd ->
+            "Unused byte after end"
+
+        Internal.ExuberantNibble ->
+            "Exuberant nibble"
+
+        Internal.ExuberantByte ->
+            "Exuberant byte"
+
+        Internal.CorruptedReservedBit ->
+            "Corrupted reserved bit"
+
+        Internal.CorruptedHuffmanHistogram ->
+            "Corrupted Huffman histogram"
+
+        Internal.CustomError message ->
+            message
+
+        Internal.UnusedSpace ->
+            "Unused space"
+
+        Internal.NoHuffmanCode symbol ->
+            "There is no huffman code for symbol " ++ String.fromInt symbol ++ ", it is outside of the alphabet!"
+
+        Internal.CorruptedContextMap ->
+            "Corrupted ContextMap"
+
+        Internal.InvalidWindowBits ->
+            "Invalid window bits"
+
+        Internal.InvalidMetablockLength ->
+            "invalid MetaBlock length"
+
+        Internal.InvalidBackwardReference message ->
+            message
+
+        Internal.UnalignedCopyBytes ->
+            "Unaligned copy bytes"
+
+        Internal.CorruptedPaddingBits ->
+            "Corrupted padding bits"
