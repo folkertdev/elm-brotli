45cd0f91725ce9fc68562db223b5f1ebddd9be4d
Folkert <folkert@folkertdev.nl>
2019-07-09 19:08:02 +0200

    clean up Transforms

--- src/Transforms.elm
+++ src/Transforms.elm
@@ -1,13 +1,14 @@
 module Transforms exposing (Transforms, new, rfc_transforms, transformDictionaryWord)
 
 import Array exposing (Array)
+import Array.Helpers
 import Bitwise
 
 
 rfc_transforms : Transforms
 rfc_transforms =
     new 121 167 50
-        |> unpackTransforms rfcPrefixSuffixSrcAlt rfcTransformsSrcAlt
+        |> unpackTransforms rfcPrefixSuffixSrc rfcTransformsSrc
 
 
 type alias Transforms =
@@ -72,16 +73,16 @@ transformDictionaryWord dst dstOffset src srcOffset_ len transforms transformInd
                     v
 
         prefix =
-            unsafeGet prefixIdx prefixSuffixHeads
+            Array.Helpers.unsafeGet prefixIdx prefixSuffixHeads
 
         prefixEnd =
-            unsafeGet (prefixIdx + 1) prefixSuffixHeads
+            Array.Helpers.unsafeGet (prefixIdx + 1) prefixSuffixHeads
 
         suffix =
-            unsafeGet suffixIdx prefixSuffixHeads
+            Array.Helpers.unsafeGet suffixIdx prefixSuffixHeads
 
         suffixEnd =
-            unsafeGet (suffixIdx + 1) prefixSuffixHeads
+            Array.Helpers.unsafeGet (suffixIdx + 1) prefixSuffixHeads
 
         omitFirst =
             let
@@ -121,7 +122,7 @@ transformDictionaryWord dst dstOffset src srcOffset_ len transforms transformInd
 
         go1 i currentOffset currentSourceOffset accum =
             if i > 0 then
-                go1 (i - 1) (currentOffset + 1) (currentSourceOffset + 1) (Array.set currentOffset (unsafeGet currentSourceOffset src) accum)
+                go1 (i - 1) (currentOffset + 1) (currentSourceOffset + 1) (Array.set currentOffset (Array.Helpers.unsafeGet currentSourceOffset src) accum)
 
             else
                 ( currentOffset, currentSourceOffset, accum )
@@ -147,23 +148,23 @@ transformDictionaryWord dst dstOffset src srcOffset_ len transforms transformInd
                     if currentLen > 0 then
                         let
                             c0 =
-                                unsafeGet uppercaseOffset accum
+                                Array.Helpers.unsafeGet uppercaseOffset accum
                         in
                         if c0 < 0xC0 then
                             go (currentLen - 1)
                                 (uppercaseOffset + 1)
                                 (if c0 >= 97 && c0 <= 122 then
-                                    update uppercaseOffset (\v -> Bitwise.xor v 32) accum
+                                    Array.Helpers.update uppercaseOffset (\v -> Bitwise.xor v 32) accum
 
                                  else
                                     accum
                                 )
 
                         else if c0 < 0x0E then
-                            go (currentLen - 2) (uppercaseOffset + 2) (update (uppercaseOffset + 1) (\v -> Bitwise.xor v 32) accum)
+                            go (currentLen - 2) (uppercaseOffset + 2) (Array.Helpers.update (uppercaseOffset + 1) (\v -> Bitwise.xor v 32) accum)
 
                         else
-                            go (currentLen - 3) (uppercaseOffset + 3) (update (uppercaseOffset + 2) (\v -> Bitwise.xor v 5) accum)
+                            go (currentLen - 3) (uppercaseOffset + 3) (Array.Helpers.update (uppercaseOffset + 2) (\v -> Bitwise.xor v 5) accum)
 
                     else
                         accum
@@ -180,13 +181,13 @@ transformDictionaryWord dst dstOffset src srcOffset_ len transforms transformInd
         applyTransform2 inputLen_ currentOffset =
             let
                 param =
-                    unsafeGet transformIndex transforms.params
+                    Array.Helpers.unsafeGet transformIndex transforms.params
 
                 go currentLength shiftOffset scalar accum =
                     if currentLength > 0 then
                         let
                             c0 =
-                                unsafeGet shiftOffset accum |> Bitwise.and 0xFF
+                                Array.Helpers.unsafeGet shiftOffset accum |> Bitwise.and 0xFF
 
                             result =
                                 transform20Helper c0 scalar currentLength shiftOffset accum
@@ -237,7 +238,7 @@ transform20Helper c0 scalar len shiftOffset dst =
         if len >= 2 then
             let
                 c1 =
-                    unsafeGet (shiftOffset + 1) dst
+                    Array.Helpers.unsafeGet (shiftOffset + 1) dst
 
                 newScalar =
                     scalar + Bitwise.or (Bitwise.and c1 0x3F) (Bitwise.shiftLeftBy 6 (Bitwise.and c0 0x1F))
@@ -257,10 +258,10 @@ transform20Helper c0 scalar len shiftOffset dst =
         if len >= 3 then
             let
                 c1 =
-                    unsafeGet (shiftOffset + 1) dst
+                    Array.Helpers.unsafeGet (shiftOffset + 1) dst
 
                 c2 =
-                    unsafeGet (shiftOffset + 2) dst
+                    Array.Helpers.unsafeGet (shiftOffset + 2) dst
 
                 newScalar =
                     scalar + Bitwise.and c2 0x3F |> Bitwise.or (Bitwise.shiftLeftBy 6 (Bitwise.and c1 0x3F)) |> Bitwise.or (Bitwise.shiftLeftBy 12 (Bitwise.and c0 0x0F))
@@ -281,13 +282,13 @@ transform20Helper c0 scalar len shiftOffset dst =
         if len >= 4 then
             let
                 c1 =
-                    unsafeGet (shiftOffset + 1) dst
+                    Array.Helpers.unsafeGet (shiftOffset + 1) dst
 
                 c2 =
-                    unsafeGet (shiftOffset + 2) dst
+                    Array.Helpers.unsafeGet (shiftOffset + 2) dst
 
                 c3 =
-                    unsafeGet (shiftOffset + 3) dst
+                    Array.Helpers.unsafeGet (shiftOffset + 3) dst
 
                 newScalar =
                     scalar
@@ -324,7 +325,7 @@ unpackTransforms prefixSuffixSrc transformsSrc transforms =
             if i < n then
                 let
                     c =
-                        unsafeGet i prefixSuffixSrc
+                        Array.Helpers.unsafeGet i prefixSuffixSrc
                 in
                 if c == 35 then
                     go (i + 1) j (index + 1) accum (Array.set index j headsAccum)
@@ -340,9 +341,9 @@ unpackTransforms prefixSuffixSrc transformsSrc transforms =
                 let
                     triplet =
                         Triplet
-                            (unsafeGet i transformsSrc - 32)
-                            (unsafeGet (i + 1) transformsSrc - 32)
-                            (unsafeGet (i + 2) transformsSrc - 32)
+                            (Array.Helpers.unsafeGet i transformsSrc - 32)
+                            (Array.Helpers.unsafeGet (i + 1) transformsSrc - 32)
+                            (Array.Helpers.unsafeGet (i + 2) transformsSrc - 32)
                 in
                 go2 (i + 3) (Array.set (i // 3) triplet currentTransforms)
 
@@ -362,49 +363,18 @@ unpackTransforms prefixSuffixSrc transformsSrc transforms =
     }
 
 
-rfcPrefixSuffixSrc : String
-rfcPrefixSuffixSrc =
-    """# #s #, #e #.# the #.com/#Â\u{00A0}# of # and # in # to #"#">#
-#]# for # a # that #. # with #'# from # by #. The # on # as # is #ing #
-\t#:#ed #(# at #ly #="# of the #. This #,# not #er #al #='#ful #ive #less #est #ize #ous #"""
 
 
-rfcPrefixSuffixSrcAlt : Array Int 
-rfcPrefixSuffixSrcAlt = 
+rfcPrefixSuffixSrc : Array Int 
+rfcPrefixSuffixSrc = 
     Array.fromList [35,32,35,115,32,35,44,32,35,101,32,35,46,35,32,116,104,101,32,35,46,99,111,109,47,35,194,160,35,32,111,102,32,35,32,97,110,100,32,35,32,105,110,32,35,32,116,111,32,35,34,35,34,62,35,10,35,93,35,32,102,111,114,32,35,32,97,32,35,32,116,104,97,116,32,35,46,32,35,32,119,105,116,104,32,35,39,35,32,102,114,111,109,32,35,32,98,121,32,35,46,32,84,104,101,32,35,32,111,110,32,35,32,97,115,32,35,32,105,115,32,35,105,110,103,32,35,10,9,35,58,35,101,100,32,35,40,35,32,97,116,32,35,108,121,32,35,61,34,35,32,111,102,32,116,104,101,32,35,46,32,84,104,105,115,32,35,44,35,32,110,111,116,32,35,101,114,32,35,97,108,32,35,61,39,35,102,117,108,32,35,105,118,101,32,35,108,101,115,115,32,35,101,115,116,32,35,105,122,101,32,35,111,117,115,32,35]
 
-rfcTransformsSrc : String
+
+
+rfcTransformsSrc : Array Int 
 rfcTransformsSrc =
-    """     !! ! ,  *!  &!  " !  ) *   * -  ! # !  #!*!  +  ,$ !  -  %  .  / #   0  1 .  "   2  3!*   4%  ! # /   5  6  7  8 0  1 &   $   9 +   :  ;  < '  !=  >  ?! 4  @ 4  2  &   A *# (   B  C& ) %  ) !*# *-% A +! *.  D! %'  & E *6  F  G% ! *A *%  H! D  I!+!  J!+   K +- *4! A  L!*4  M  N +6  O!*% +.! K *G  P +%(  ! G *D +D  Q +# *K!*G!+D!+# +G +A +4!+% +K!+4!*D!+K!*K"""
-
-
-rfcTransformsSrcAlt : Array Int 
-rfcTransformsSrcAlt =
     Array.fromList
 
     [32,32,32,32,32,33,33,32,33,32,44,32,32,42,33,32,32,38,33,32,32,34,32,33,32,32,41,32,42,32,32,32,42,32,45,32,32,33,32,35,32,33,32,32,35,33,42,33,32,32,43,32,32,44,36,32,33,32,32,45,32,32,37,32,32,46,32,32,47,32,35,32,32,32,48,32,32,49,32,46,32,32,34,32,32,32,50,32,32,51,33,42,32,32,32,52,37,32,32,33,32,35,32,47,32,32,32,53,32,32,54,32,32,55,32,32,56,32,48,32,32,49,32,38,32,32,32,36,32,32,32,57,32,43,32,32,32,58,32,32,59,32,32,60,32,39,32,32,33,61,32,32,62,32,32,63,33,32,52,32,32,64,32,52,32,32,50,32,32,38,32,32,32,65,32,42,35,32,40,32,32,32,66,32,32,67,38,32,41,32,37,32,32,41,32,33,42,35,32,42,45,37,32,65,32,43,33,32,42,46,32,32,68,33,32,37,39,32,32,38,32,69,32,42,54,32,32,70,32,32,71,37,32,33,32,42,65,32,42,37,32,32,72,33,32,68,32,32,73,33,43,33,32,32,74,33,43,32,32,32,75,32,43,45,32,42,52,33,32,65,32,32,76,33,42,52,32,32,77,32,32,78,32,43,54,32,32,79,33,42,37,32,43,46,33,32,75,32,42,71,32,32,80,32,43,37,40,32,32,33,32,71,32,42,68,32,43,68,32,32,81,32,43,35,32,42,75,33,42,71,33,43,68,33,43,35,32,43,71,32,43,65,32,43,52,33,43,37,32,43,75,33,43,52,33,42,68,33,43,75,33,42,75]
 
-charCodeAt : Int -> String -> Int
-charCodeAt n str =
-    case String.uncons (String.dropLeft n str) of
-        Nothing ->
-            0
 
-        Just ( c, _ ) ->
-            Char.toCode c
-
-
-unsafeGet : Int -> Array Int -> Int
-unsafeGet i arr =
-    Array.get i arr
-        |> Maybe.withDefault 0
-
-
-update : Int -> (a -> a) -> Array a -> Array a
-update index f arr =
-    case Array.get index arr of
-        Nothing ->
-            arr
-
-        Just v ->
-            Array.set index (f v) arr
